ef377c973a1d74a75b45607a9b303953011ed8f1
spiritscout <spdx5651@leeds.ac.uk>
2025-10-10 22:31:38 +0000

    Task 5

--- tasks/task5_2_1/Circle.kt
+++ tasks/task5_2_1/Circle.kt
@@ -1,1 +1,23 @@
 // Task 5.2.1: geometric properties of circles
+
+import kotlin.math.PI
+
+fun circleArea(radius: Double) = PI * radius * radius
+
+
+fun circlePerim(radius: Double) = PI * radius * 2
+
+
+fun readDouble(prompt: String) : Double {
+    println(prompt)
+    return readln().toDouble()
+}
+
+fun main() {
+    val prompt = "Please Enter The Radius:"
+    val radius = readDouble(prompt)
+    val area = circleArea(radius)
+    val perim = circlePerim(radius)
+    println("The Area is: $area")
+    println("The Circumference is: $perim")
+}
