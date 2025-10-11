173635134dbc04deda067b890a31d0cf1d5c6f3f
spiritscout <spdx5651@leeds.ac.uk>
2025-10-11 14:38:22 +0000

    task 5.4

--- tasks/task5_2_2/Grades.kt
+++ tasks/task5_2_2/Grades.kt
@@ -1,1 +1,17 @@
 // Task 5.2.2: conversion of marks into grades, using a function
+
+fun main(args: Array<String>) {
+    for (n in 0..(args.size - 1)) {
+        var mark = args[n].toInt()
+        var wGrade = grade(mark)
+        println("$mark is a $wGrade")
+    }
+}
+
+
+fun grade(mark: Int) = when (mark) {
+    in 0..39   -> "Fail"
+    in 40..69  -> "Pass"
+    in 70..100 -> "Distinction"
+    else       -> "?"
+}
--- tasks/task5_3_1/Die2.kt
+++ tasks/task5_3_1/Die2.kt
@@ -1,34 +1,32 @@
-// Task 5.3.2: dice rolling simulation
+// Task 5.1.2: die rolling simulation
 import kotlin.random.Random
-
 
 fun main() {
     val prompt = "Enter a Die Size 4-20, or leave blank for d6"
     val sides = readInt(prompt)
-    println("Enter Number of Dice: ")
-    val dice = readln().toInt()
     if (sides != null) {
-        rollDie(sides, dice)
+        rollDie(sides)
     } else {
-        rollDie(dice = dice)
+        rollDie()
     }
-    
+
 }
 
 
 
 
-fun rollDie(sides: Int = 6, dice: Int) {
+fun rollDie(sides: Int = 6) {
     if (sides in setOf(4, 6, 8, 10, 12, 20)) {
         println("Rolling a d$sides...")
         val result = Random.nextInt(1, sides + 1)
-        val total = result * dice
-        println("You rolled a score of $total")
+        println("You rolled $result")
     }
     else {
         println("Error: cannot have a $sides-sided die")
     }
 }
+
+
 
 
 fun readInt(prompt: String): Int? {
