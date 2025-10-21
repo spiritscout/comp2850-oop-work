e697611b3a1ad33698260ef4713bf7f478e583f8
spiritscout <spdx5651@leeds.ac.uk>
2025-10-21 19:23:27 +0000

    Wordle Funcs

--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,1 +1,33 @@
 // Implement the six required functions here
+import java.io.File
+import kotlin.system.exitProcess
+import kotlin.random.Random
+
+
+//Check input is exactly 5 letters 
+ fun isValid(word: String): Boolean {
+    if (word.length == 5 && word.all {it.isLetter()}) {
+        return true
+    } else {
+        return false
+    }
+ }
+
+//read target words from file, return them as a list of strings
+fun readWordList(filename: String): MutableList<String> {
+    val wordList = mutableListOf<String>() 
+    File(filename).forEachLine {wordList.add(it)}
+    return wordList 
+}
+
+//Choose a random word from given lsit then returns it
+fun pickRandomWord(words: MutableList<String>): String {
+    val rndint = Random.nextInt(0, words.size + 1)          //random int corresponding to a list entry
+    val selectedWord = words[rndint]                        //entry at selected int
+    return selectedWord
+}
+
+fun obtainGuess(attempt: Int): String {
+    println("Attempt $attempt: ")
+    readln().uppercase()
+}
