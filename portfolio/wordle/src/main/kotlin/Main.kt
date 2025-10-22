65367811e9cdf0d2069900484b0303c87afb35e0
spiritscout <spdx5651@leeds.ac.uk>
2025-10-22 22:24:04 +0000

    Wordle Main

--- portfolio/wordle/src/main/kotlin/Main.kt
+++ portfolio/wordle/src/main/kotlin/Main.kt
@@ -1,2 +1,39 @@
+import java.io.File
+import kotlin.system.exitProcess
+import kotlin.random.Random
+
+
 fun main() {
-}
+    
+    val filename = "data/words.txt"
+    val wordList: MutableList<String>
+
+    try {
+        wordList = readWordList(filename)
+    }
+    catch (error: Exception) {
+        println("Unable to read from file words.txt")
+    }
+    
+
+    val target = pickRandomWord(wordList)
+    var i = 1
+    while (i <= 10) {
+        val attempt = i
+        val guess = obtainGuess(attempt)
+        val matches = evaluateGuess(guess, target)
+        displayGuess(guess, matches)
+
+        if (matches.all { it == 1 }) {
+            println("Congratulations!")
+            break
+        } else {
+            i++
+        }
+    }
+    if (i > 10) {
+    println("Sorry, you are out of guesses")
+    println("The word was: $target")
+    }
+}   
+
--- portfolio/wordle/src/main/kotlin/Wordle.kt
+++ portfolio/wordle/src/main/kotlin/Wordle.kt
@@ -1,12 +1,12 @@
 // Implement the six required functions here
-import java.io.File
-import kotlin.system.exitProcess
-import kotlin.random.Random
+
+
 
 
 //Check input is exactly 5 letters 
  fun isValid(word: String): Boolean {
-    if (word.length == 5 && word.all {it.isLetter()}) {
+    val wordtrim = word.trim()
+    if (wordtrim.length == 5 && wordtrim.all {it.isLetter()}) {
         return true
     } else {
         return false
@@ -23,18 +23,21 @@ fun readWordList(filename: String): MutableList<String> {
 //Choose a random word from given lsit then returns it
 fun pickRandomWord(words: MutableList<String>): String {
     val rndint = Random.nextInt(0, words.size + 1)          //random int corresponding to a list entry
-    val selectedWord = words[rndint]                        //entry at selected int
+    val selectedWord = words[rndint]                        //entry at selected int 
+    words.remove(selectedWord)
     return selectedWord
 }
 
 //Prompt the User for input then return the guess once validated
 fun obtainGuess(attempt: Int): String {
     println("Attempt $attempt: ")
-    var guess = readln().uppercase()
+    val input = readlnOrNull()?.trim() ?: ""
+    var guess = input.uppercase()
     while (isValid(guess) == false) {                       //check guess is valid
         println("Please enter a 5 letter word")     
         println("Attempt $attempt: ")                       //reprompt if invalid
-        guess = readln().uppercase()
+        val input = readlnOrNull()?.trim() ?: ""
+        var guess = input.uppercase()
     }
     return guess
 }
@@ -42,5 +45,29 @@ fun obtainGuess(attempt: Int): String {
 
 //Compare given guess with target word 
 fun evaluateGuess(guess: String, target: String): List<Int> {
-    
+    val guessArray = guess.toCharArray()
+    val targetArray = target.toCharArray()
+    val compareList = mutableListOf<Int>()
+    for (n in 0 .. 4) {
+        if (guessArray[n] == targetArray[n]) {
+            compareList.add(1)
+        }   else {
+            compareList.add(0)
+        }
+    } 
+    return compareList
+}
+
+//Display the letters of a guess that match the target word
+fun displayGuess(guess: String, matches: List<Int>) {
+    val guessArray = guess.toCharArray()
+    for (n in 0 .. 4) {
+        if (matches[n] == 1) {
+            //nothing
+        } else {
+            guessArray[n] = '?'
+        }
+    }
+    val guessString = String(guessArray)
+    println(guessString)
 }
